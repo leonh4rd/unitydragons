05c44a39bb2df19bae139003cab37606b737f184
Leonardo <leonardo@quetzal.com.br>
2019-06-07 11:01:54 -0300

    Add dragon collision responses

---
+++ Assets/Scripts/DragonKiller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragonKiller : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(transform.position.y > 6f || transform.position.y < -5.6f)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        SceneManager.LoadScene(0);
+    }
+}
