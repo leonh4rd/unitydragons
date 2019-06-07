f535055d63807ac3aa20c83d0e391f906a99cf80
Leonardo <leonardo@quetzal.com.br>
2019-06-07 10:41:13 -0300

    Add jump capability to dragon

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Jump : MonoBehaviour
+{
+    public Vector2 jumpForce;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        }
+    }
+}
