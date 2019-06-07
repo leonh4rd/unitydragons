8a8339cfed0dfa6654b6194ed3365b171105b099
Leonardo <leonardo@quetzal.com.br>
2019-06-07 11:59:33 -0300

    Create Tree prefab

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,18 +6,30 @@ public class MoveLeft : MonoBehaviour
 {
     public float moveSpeed = 1f;
 
-    void Start()
-    {
-        
+    private void OnEnable() {
+        ShowRandomSprite();
     }
 
     void Update()
     {
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
 
-        if(transform.position.x < -15)
+        if(transform.position.x < -10)
         {
-            transform.position += Vector3.right * 30f;
+            transform.position += Vector3.right * 25f;
+            ShowRandomSprite();
+        }
+    }
+
+    private void ShowRandomSprite()
+    {
+        int index = Random.Range(0, 3);
+        int childCount = transform.childCount;
+
+        for(int i = 0; i < childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            child.gameObject.SetActive(index == i);
         }
     }
 }
