1967aa9b9469c8167a99fcb838ad279c14030880
Leonardo <leonardo@quetzal.com.br>
2019-06-07 11:25:59 -0300

    Add movement to stumps

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        if(transform.position.x < -15)
+        {
+            transform.position += Vector3.right * 30f;
+        }
+    }
+}
